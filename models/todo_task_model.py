08788b7306de3257e0b19231a350155206034960
Paszo <paszo2008@gmail.com>
2019-02-22 18:41:41 +0100

    [ADD] Added model todo.task

---
+++ models/todo_task_model.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    _description = 'To-do Task'
+    name = fields.Char('Description', required=True)
+    is_done = fields.Boolean('Done?')
+    active = fields.Boolean('Active?', default=True)
+    date_deadline = fields.Date('Deadline')
+    user_id = fields.Many2one(
+        'res.users',
+        string='Responsible',
+        default=lambda self: self.env.user)
+    team_ids = fields.Many2many('res.partner', string='Team')
